a7571147e6ad2fae614338919a6cda5924acea26
Loïc Mathieu <loikeseke@gmail.com>
2025-03-19 09:48:14 +0100

    feat: add package-info.java

---
+++ src/main/java/io/kestra/plugin/templates/package-info.java
@@ -0,0 +1,7 @@
+@PluginSubGroup(
+    title = "Example plugin",
+    description = "A plugin to show how to build a plugin in Kestra."
+)
+package io.kestra.plugin.templates;
+
+import io.kestra.core.models.annotations.PluginSubGroup;
