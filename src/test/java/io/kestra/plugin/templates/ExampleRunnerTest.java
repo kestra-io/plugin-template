5498cc56426ab1bdf9cacbb1c926a1066981463c
Loïc Mathieu <loikeseke@gmail.com>
2025-09-04 15:54:24 +0200

    chore: cleanup the unit test

--- src/test/java/io/kestra/plugin/templates/ExampleRunnerTest.java
+++ src/test/java/io/kestra/plugin/templates/ExampleRunnerTest.java
@@ -2,37 +2,24 @@ package io.kestra.plugin.templates;
 
 import io.kestra.core.junit.annotations.ExecuteFlow;
 import io.kestra.core.junit.annotations.KestraTest;
-import io.kestra.core.queues.QueueException;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import io.kestra.core.models.executions.Execution;
-import io.kestra.core.repositories.LocalFlowRepositoryLoader;
-import io.kestra.core.runners.RunnerUtils;
-import io.kestra.core.runners.StandAloneRunner;
 
-import jakarta.inject.Inject;
-import java.io.IOException;
-import java.net.URISyntaxException;
 import java.util.Map;
-import java.util.Objects;
-import java.util.concurrent.TimeoutException;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.Matchers.is;
 
 /**
- * This test will load all flow located in `src/test/resources/flows/`
- * and will run an in-memory runner to be able to test a full flow. There is also a
- * configuration file in `src/test/resources/application.yml` that is only for the full runner
- * test to configure in-memory runner.
+ * This test will start Kestra, load the flow located in `src/test/resources/flows/example.yaml`, and execute it.
+ * The Kestra configuration file is in `src/test/resources/application.yml`, it configures the in-memory backend for tests.
  */
-@KestraTest(startRunner = true)
+@KestraTest(startRunner = true) // This annotation starts an embedded Kestra for tests
 class ExampleRunnerTest {
-    @SuppressWarnings("unchecked")
     @Test
     @ExecuteFlow("flows/example.yaml")
-    void flow(Execution execution) throws TimeoutException, QueueException {
+    void flow(Execution execution) {
         assertThat(execution.getTaskRunList(), hasSize(3));
         assertThat(((Map<String, Object>)execution.getTaskRunList().get(2).getOutputs().get("child")).get("value"), is("task-id"));
     }
